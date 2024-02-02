cbda9227339ddec5b5fb4a2830a396c0d404a853
DaniCalderon <mesiasd.calderon@gmail.com>
2024-02-01 21:46:39 -0300

    Añadiendo Footer y mejorando documentacion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// NAVBAR
 // Con este script logramos que el navbar sea transparente al inicio, y que al hacer scroll
 // en cuanto este sea mayor que 100, el navbar cambiara a color negro.
 var nav = $('nav');
@@ -9,14 +10,16 @@ $(window).on('scroll', function () {
     }
 });
 
+// QUIENES SOMOS
 //  Con este script logramos que se genere el toggle slow en todos los parrafos, al momento de hacer click
 // en cualquiera de los tres titulos de los parrafos antes mencionados
 $(".fondot").click(function () {
     $(".tg").toggle("slow");
 });
 
-//  Este script tiene como función mostrarnos un mensaje desde la consola de nuestro navegador, que 
-// al hacer click sobre el boton "Enviar", nos indicara que el mensaje fue enviado con exito
+// FORMULARIO
+//  Este script tiene como función mostrarnos un alert, y al hacer click 
+// sobre el boton "Enviar", nos indicara que el mensaje fue enviado con exito
 $("#enviar").click(function () {
     alert("Su mensaje fue enviado correctamente~");
 });
